000197a67f2b92b1528f190ba34c4b7bbdcd816e
kitutune <p.kitutune@gmail.com>
2022-08-28 20:06:38 +0900

    リファクタ

--- app/fe/service/GET/useGetTodoList.ts
+++ app/fe/service/GET/useGetTodoList.ts
@@ -2,12 +2,12 @@ import axios, { AxiosResponse } from "axios";
 import { useCallback } from "react";
 import { useSetRecoilState } from "recoil";
 import { todoListState } from "../../atom/GET/TodoList";
-export const useGetUserList = () => {
+export const useGetTodoList = () => {
   // java側でGETメソッドを実装しているURL、リクエスト先
   const BASEURL = "http://localhost:8080/api/all";
   // 状態を入力する側のRecoil
   const setRecoilTodoList = useSetRecoilState(todoListState);
-  // userテーブルから取得
+  // Todoテーブルから取得
   const getTodoList = useCallback(async () => {
     const response: AxiosResponse = await axios.get(BASEURL).catch((error) => {
       // レスポンスありのエラーハンドリング（実際には必要に応じた例外処理を実装する）
@@ -22,7 +22,7 @@ export const useGetUserList = () => {
     setRecoilTodoList(response.data);
     // 回収していないがAxiosResponseの型用に配置
     return response.data;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return getTodoList;
