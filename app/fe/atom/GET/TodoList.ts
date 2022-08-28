3a0697ca6e0bb1ca9040ff612caa55f5cd956700
kitutune <p.kitutune@gmail.com>
2022-08-28 10:57:40 +0900

    Recoil実装

---
+++ app/fe/atom/GET/TodoList.ts
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+// バケツリレーをしないためにRecoilを採用
+// 編集するユーザーのデータを管理する変数を作成する
+export const todoListState = atom({
+  key: "editUserAtom",
+  default: [
+    {
+      // ユニーク
+      id: "",
+      // todoの作成日（編集時に更新するかはまだ未定）
+      productionDate: "",
+      // todoの最終期限
+      finalDeadline: "",
+      // やること
+      todo: "",
+      // 作業済み
+      isDone: "",
+      // 重要度
+      priority: "",
+    },
+  ],
+});
