22ee15ec6fc3e3f821a19fe7f83a4b67f9ad666f
kitutune <p.kitutune@gmail.com>
2022-08-30 07:33:44 +0900

    TodoのALLDELETEボタンを作成

---
+++ app/fe/service/Delete/useDeleteAllTodo.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const useDeleteAllTodo = () => {
+  const deleteAllTodo = () => {
+    axios.delete(`http://localhost:8080/api/alldelete`).then((res) => {
+      console.log("res.data", res.data);
+    });
+  };
+
+  return deleteAllTodo;
+};
