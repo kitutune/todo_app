d7404092609ffc27e49205b2449e7d1caf51e1c1
kitutune <p.kitutune@gmail.com>
2022-09-03 18:06:08 +0900

    DB取得フラグをCRUDメソッドに設定

--- app/fe/service/Put/usePutTodo.ts
+++ app/fe/service/Put/usePutTodo.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useCallback } from "react";
 import { TodoFormValue } from "../../types/todo";
+import { useGetTodoList } from "../GET/useGetTodoList";
 
 // useHookであるusePutTodo自体には引数は必要ない
 export const usePutTodo = () => {
+  //useHook
+  const isFetchDBTrue = useGetTodoList();
   // DBにformから受け取った値を登録（INSERT）するメソッド（PUTなので既に登録されているデータに上書きする）
   // dbEditedには引数が必要
   const dbEdited = useCallback((formTodo: TodoFormValue) => {
@@ -18,11 +21,14 @@ export const usePutTodo = () => {
       .then((response) => {
         if (response.status === 200) {
           console.log("登録成功");
+          //  正常にDBにデータを格納したのでDB再取得フラグをtrueに設定
+          isFetchDBTrue();
         }
 
         console.log(response.status);
         console.log(response.data);
       });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return dbEdited;
