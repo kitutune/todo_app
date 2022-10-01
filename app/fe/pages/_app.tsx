b1c2737332c6740242e530851307cebca8e12fdc
kitutune <p.kitutune@gmail.com>
2022-10-01 10:41:59 +0900

    大文字小文字のファイルネームの変更と型の名前変更

--- app/fe/pages/_app.tsx
+++ app/fe/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { Layout } from "../components/Layout/Layout";
 import { Center } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import { Layout } from "components/layout/Layout";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 
--- app/fe/service/edit/useEditTodo.ts
+++ app/fe/service/edit/useEditTodo.ts
@@ -1,13 +1,13 @@
-import { TodoFormValueType } from "../../types/todo";
+import { TodoBeValueType } from "../../types/todo";
 import axios from "axios";
 import { useCallback } from "react";
-import { useErrorHandle } from "service/ErrorHandle/useErrorHandle";
+import { useErrorHandle } from "service/errorHandle/useErrorHandle";
 
 export const useEditTodo = () => {
   // console.log("usePutTodo");
   const axiosError = useErrorHandle();
   const dbEdited = useCallback(
-    async (formTodo: TodoFormValueType) => {
+    async (formTodo: TodoBeValueType) => {
       try {
         await axios.put(
           `http://localhost:8080/api/edit/${formTodo.id}`,
