810d407fa4cde7ecf26deeac84b227943ac2f24e
kitutune <p.kitutune@gmail.com>
2022-10-08 13:47:40 +0900

    BEとFEのDate型の受け渡しを全てStringに変換してから行うように修正

--- app/fe/service/Convert/useConvert.ts
+++ app/fe/service/Convert/useConvert.ts
@@ -1,44 +1,50 @@
 import { TodoBeValueType, TodoFormValueType } from "../../types/todo";
+import { useFormatDate } from "./useFormatDate";
 import { useCallback } from "react";
 
 export const useConvert = () => {
   // console.log("useConvert");
-
-  const convertFormFEToBE = useCallback((formTodo: TodoFormValueType) => {
-    return {
-      id: formTodo.id,
-      productionDate: formTodo.productionDate,
-      finalDeadline: formTodo.finalDeadline,
-      todo: formTodo.todo,
-      isDone: String(formTodo.isDone),
-      priority: String(formTodo.priority),
-    };
-  }, []);
+  const { dateToStr } = useFormatDate();
+  const convertFormFEToBE = useCallback(
+    (formTodo: TodoFormValueType) => {
+      return {
+        id: formTodo.id,
+        productionDate: dateToStr(formTodo.productionDate),
+        finalDeadline: dateToStr(formTodo.finalDeadline),
+        todo: formTodo.todo,
+        isDone: String(formTodo.isDone),
+        priority: String(formTodo.priority),
+      };
+    },
+    [dateToStr]
+  );
 
   const convertBEtoFE = useCallback((beTodoLists: TodoBeValueType[]) => {
     if (beTodoLists === undefined) {
       return beTodoLists;
     }
 
-    const feTodoList = beTodoLists.map((beTodo) => {
-      // setValuesの中で三項演算子を使いたく無いので
-      let feIsDone: boolean;
-      if (beTodo.isDone.toString() === "true") {
-        feIsDone = true;
-      } else {
-        feIsDone = false;
-      }
-      return {
-        id: beTodo.id,
-        productionDate: beTodo.productionDate,
-        finalDeadline: beTodo.finalDeadline,
-        todo: beTodo.todo,
-        isDone: feIsDone,
-        priority: Number(beTodo.priority),
-      };
-    });
-    return feTodoList;
-  }, []);
+      const feTodoList = beTodoLists.map((beTodo) => {
+        // setValuesの中で三項演算子を使いたく無いので
+        let feIsDone: boolean;
+        if (beTodo.isDone.toString() === "true") {
+          feIsDone = true;
+        } else {
+          feIsDone = false;
+        }
+        return {
+          id: beTodo.id,
+          productionDate: new Date(beTodo.productionDate),
+          finalDeadline: new Date(beTodo.finalDeadline),
+          todo: beTodo.todo,
+          isDone: feIsDone,
+          priority: Number(beTodo.priority),
+        };
+      });
+      return feTodoList;
+    },
+    []
+  );
 
   return { convertFormFEToBE, convertBEtoFE };
 };
