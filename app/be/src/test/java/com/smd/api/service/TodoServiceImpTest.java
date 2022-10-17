b5bbc3babd25414a47fdbc10649be0004a347bbd
kitutune <p.kitutune@gmail.com>
2022-10-18 07:40:10 +0900

    現状の状態：コンテナ内でエラー

---
+++ app/be/src/test/java/com/smd/api/service/TodoServiceImpTest.java
@@ -0,0 +1,35 @@
+package com.smd.api.service;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+// import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import com.smd.api.entity.TodoEntity;
+import com.smd.api.form.TodoForm;
+import com.smd.api.repository.TodoRepository;
+
+// class TodoServiceImpTest {}
+
+//	@AfterAll
+//	static void tearDownAfterClass() throws Exception {
+//	}
+// テスト対象
+
+public class TodoServiceImpTest {  
+	@Test  
+	public void testCollectionSize() {  
+	    List<String> catNames = Arrays.asList("Phibi", "Monica");  
+	  
+	    assertThat(catNames, hasSize(2));  
+	}
+}
+
+
