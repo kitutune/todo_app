e76f3e0a4220545cb0ecbcf5ef450965e09adbb0
kitutune <p.kitutune@gmail.com>
2022-10-19 08:00:38 +0900

    現在のエラー問題２件発生時点での状態

--- app/be/src/test/java/com/smd/api/service/TodoServiceImpTest.java
+++ app/be/src/test/java/com/smd/api/service/TodoServiceImpTest.java
@@ -1,33 +1,16 @@
 package com.smd.api.service;
 
-import org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.Arrays;
-// import static org.hamcrest.MatcherAssert.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import com.smd.api.entity.TodoEntity;
-import com.smd.api.form.TodoForm;
-import com.smd.api.repository.TodoRepository;
-
-// class TodoServiceImpTest {}
-
-//	@AfterAll
-//	static void tearDownAfterClass() throws Exception {
-//	}
-// テスト対象
+import java.util.Arrays;
+import java.util.List;
 
 public class TodoServiceImpTest {
 	@Test
 	public void testCollectionSize() {
-		List<String> catNames = Arrays.asList("Phibi", "Monica");
+		List<String> catNames = Arrays.asList("Phibi", "Monica","kkkk");
 
-		assertThat(catNames, hasSize(2));
+		Assertions.assertThat(catNames).hasSize(2);
 	}
 }
